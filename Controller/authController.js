eb655759fa17d1c82656a023fdfaa71a7ce221b7
imrg1999 <rupankar.ghosh1999@gmail.com>
2025-11-11 01:23:02 +0530

    login

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,7 +1,9 @@
-import { ZodError } from "zod";
+import { success, ZodError } from "zod";
 import userModel from "../Model/userModel.js";
 import { passwordHashing } from "../Utility/passwordHashing.js";
 import zodSchema  from "../Utility/zodSchema.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const registerUser = async(req,res) => {
     try{
@@ -28,6 +30,46 @@ export const registerUser = async(req,res) => {
             success: true,
             message: "Registration Successful",
             user: registeredNewUser
+        })
+    } catch(error) {
+        if(error instanceof ZodError) {
+            return res.status(400).json({
+                success: false,
+                error: error.issues
+            })
+        }
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        })
+    }
+}
+
+export const logInUser = async(req,res) => {
+    try{
+        const logInReqFormat = await zodSchema.parseAsync(req.body);
+        const registered = await userModel.findOne({email: logInReqFormat.email});
+        if(!registered) {
+            return res.status(400).json({
+                success: false,
+                message: "The User isn't registered yet"
+            })
+        }
+        
+        const validUser = await bcrypt.compare(logInReqFormat.password, registered.password);
+        const token = jwt.sign({id: registered._id, email: registered.email},
+            process.env.JWT_SECRET, {expiresIn: '1h'}
+        )
+        if(!validUser) {
+        return res.status(400).json({
+                success: false,
+                message: "Invalid User"
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "Valid User",
+            token: token
         })
     } catch(error) {
         if(error instanceof ZodError) {
--- Route/authRoutes.js
+++ Route/authRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { registerUser } from '../Controller/authController.js';
+import { registerUser, logInUser } from '../Controller/authController.js';
 
 const route = express.Router();
 
 route.post('/register',registerUser);
-
+route.post('/login',logInUser);
 
 export default route;
